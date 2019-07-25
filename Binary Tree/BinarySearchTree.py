209f1cd9fc5818b393392ff483e877515121f755
Karol Oleszek <oleszek.karol@gmail.com>
2019-07-25 20:27:23 +0200

    Binary search tree

---
+++ Binary Tree/BinarySearchTree.py
@@ -0,0 +1,95 @@
+"""Binary Search Tree Python implementation."""
+
+
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value > self.key:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = Node(value)
+        elif value < self.key:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = Node(value)
+
+    def search(self, value):
+        if value == self.key:
+            return True
+        elif value > self.key:
+            if self.right:
+                return self.right.search(value)
+            else:
+                return False
+        else:
+            if self.left:
+                return self.left.search(value)
+            else:
+                return False
+
+    def inorder(self):
+        if self.left:
+            self.left.inorder()
+        print(self.key)
+        if self.right:
+            self.right.inorder()
+
+    def minimum_node(self):
+        current = self
+        while current.left:
+            current = current.left
+        return current
+
+    def minimum_and_preceding_node(self):
+        current = self
+        previous = None
+        while current.left:
+            previous = current
+            current = current.left
+        return current, previous
+
+    def find_pred_and_succ(self, value, prv=None):
+        if value == self.key:
+            return prv, self
+        elif value > self.key:
+            if self.right:
+                return self.right.find_pred_and_succ(value, self)
+        else:
+            if self.left:
+                return self.left.find_pred_and_succ(value, self)
+
+    def kth_smallest(self, k):
+        if k:
+            if self.left:
+                self.left.kth_smallest(k)
+                k -= 1
+        if k:
+            k -= 1
+            print(self.key)
+        if k:
+            if self.right:
+                self.right.kth_smallest(k)
+                k -= 1
+
+
+if __name__ == "__main__":
+    root = Node(50)
+    root.insert(30)
+    root.insert(20)
+    root.insert(40)
+    root.insert(70)
+    root.insert(60)
+    root.insert(80)
+    print("100", root.search(100))
+    print("70", root.search(70))
+    root.inorder()
+    print(root.minimum_node().key)
+    print([x.key for x in root.minimum_and_preceding_node()])
+    print([x.key for x in root.find_pred_and_succ(40)])
+    root.kth_smallest(4)
