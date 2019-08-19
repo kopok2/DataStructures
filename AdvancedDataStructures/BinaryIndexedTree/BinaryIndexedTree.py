9e6b946e519b7e49174c8d6e4250fc5ce3272361
kopok2 <oleszek.karol@gmail.com>
2019-08-19 12:16:02 +0000

    Binary indexed tree expansion

--- AdvancedDataStructures/BinaryIndexedTree/BinaryIndexedTree.py
+++ AdvancedDataStructures/BinaryIndexedTree/BinaryIndexedTree.py
@@ -12,7 +12,7 @@ class BinaryIndexedTree:
     def update_bit(self, i, v):
         i += 1
         while i <= len(self.array):
-            self.bit [i] += v
+            self.bit[i] += v
             i += i & (-i)
 
     def get_sum(self, i):
@@ -23,9 +23,25 @@ class BinaryIndexedTree:
             i -= i & (-i)
         return s
 
+    def update_range(self, l, r, val):
+        for i in range(l, r + 1):
+            self.update_bit(i, val)
+
+    def get_element(self, i):
+        return self.get_sum(i) - self.get_sum(i - 1)
+
+    def get_range_sum(self, l, r):
+        return self.get_sum(r - 1) - self.get_sum(l - 1)
+
 
 if __name__ == "__main__":
     array = [2, 1, 1, 3, 2, 3, 4, 5, 6, 7, 8, 9]
     bit = BinaryIndexedTree(array)
     for x in range(len(array)):
         print(sum(array[:x + 1]), bit.get_sum(x))
+    print([bit.get_element(x) for x in range(len(array))], array)
+    print(bit.get_range_sum(0, len(array)), sum(array))
+    print(bit.get_range_sum(0, len(array) // 2), sum(array[:len(array) // 2]))
+    bit.update_range(0, len(array) - 1, 1)
+    print([bit.get_element(x) for x in range(len(array))], array)
+
